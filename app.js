0258f2f032899ebd389bcb587314eb6f0f0991b6
mnrazablog <mnraza94@gmail.com>
2024-06-09 12:39:39 +0530

    used routes using express js

--- app.js
+++ app.js
@@ -9,9 +9,36 @@ app.get("/social", (req, res)=>{
     res.send("Welcome to LinkedIN World!")
 })
 
-app.get("/test", (req, res)=>{
-    res.send("Welcome to Test World!")
+app.get("/insta", (req, res)=>{
+    const insta = {
+        userName:"hiteshsir",
+        followers:70,
+        follow:200,
+    };
+    res.status(200).json({insta})
 })
+app.get("/twitter", (req, res)=>{
+    const twitter = {
+        userName:"hiteshsir",
+        followers:700,
+        follow:2080,
+    };
+    res.status(200).json({twitter})
+})
+app.get("/linkedin", (req, res)=>{
+    const linkedin = {
+        userName:"hiteshsir",
+        followers:710,
+        follow:2000,
+    };
+    res.status(200).json({linkedin})
+})
+
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`I am able to listen at port ${PORT}`);
